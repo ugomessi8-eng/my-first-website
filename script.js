264e8fdb8e9708d05a9d5d53fb32f4c5aacbd54c
ugomessi8-eng <ugomessi8@gmail.com>
2026-02-03 11:31:11 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ for (let i = 0; i < skills.length; i++) {
   li.textContent = skills[i];
   list.appendChild(li);
 }
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+}
+function changeBackground() {
+  const colors = ["#ffadad", "#ffd6a5", "#fdffb6", "#caffbf", "#9bf6ff", "#bdb2ff"];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[randomIndex];
+}
